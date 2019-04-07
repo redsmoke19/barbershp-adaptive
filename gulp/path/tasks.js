02b66943defaf231148e8df8da5572d207f916c2
Oleg Tsyganov <TsyganovDev@gmail.com>
2019-04-07 15:26:42 +0400

    first commit

---
+++ gulp/path/tasks.js
@@ -0,0 +1,10 @@
+module.exports = [
+  './gulp/tasks/clean',
+  './gulp/tasks/fonts',
+  './gulp/tasks/pug',
+  './gulp/tasks/browserSync',
+  './gulp/tasks/styles',
+  './gulp/tasks/svg',
+  './gulp/tasks/image',
+  './gulp/tasks/watch'
+];
